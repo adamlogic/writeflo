532e953c3e58ff25b9ab0214d00cc45fa4a1993a
Adam McCrea <adam@adamlogic.com>
2013-03-17 08:39:41 -0400

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Writeflo::Application.config.secret_token = '58cc20b3105ea151db0d696aa3a402b40137ac77d55c02699111807fba51c04d7de0b8e82e37b42c40f96ee9cf49aeeb229288eb06213171f969587d6404505a'
