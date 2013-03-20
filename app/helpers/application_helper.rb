39c43ac62959c6c41f9e3fa1285a0911b9f254c4
Adam McCrea <adam@adamlogic.com>
2013-03-19 21:08:45 -0400

    Allow selections across paragraphs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,12 +2,8 @@ module ApplicationHelper
   def content_to_html(text)
     text.gsub!(/\r\n?/, "\n") # normalize line breaks
 
-    html = text.split(/\n\n+/).map do |paragraph|
-      paragraph.scan(/\w+|<[^>]*>|./).map do |chunk|
-        "<span>#{chunk}</span>"
-      end.join
-    end.map do |paragraph|
-      "<p>#{paragraph}</p>"
+    html = text.scan(/\w+|<[^>]*>|\n+|./).map do |word|
+      content_tag :span, word, word_span_attrs(word)
     end.join
 
     html.html_safe
@@ -15,5 +11,11 @@ module ApplicationHelper
     #TODO: handle single line breaks
     #TODO: encode html in the content
     # html.gsub!(/([^\n]\n)(?=[^\n])/, '\1<br />') # 1 newline   -> br
+  end
+
+  def word_span_attrs(word)
+    if word == "\n\n"
+      {class: 'paragraph-break'}
+    end
   end
 end
