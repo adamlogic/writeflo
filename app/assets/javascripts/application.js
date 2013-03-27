9d75390356fb4661782533026bb668d672422d86
Adam McCrea <adam@adamlogic.com>
2013-03-27 17:21:54 -0400

    Place the cursor at the end of the doc

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,18 @@ $(function() {
   var $doc = $('#doc-content');
   var padding = $doc.outerHeight() - $doc.height();
   $doc.height($(window).height() - padding);
+  $doc.focusAtEndOfText();
 });
+
+$.fn.focusAtEndOfText = function () {
+  var el = this[0];
+
+  if (typeof el.selectionStart == "number") {
+    el.selectionStart = el.selectionEnd = el.value.length;
+  } else if (typeof el.createTextRange != "undefined") {
+    el.focus();
+    var range = el.createTextRange();
+    range.collapse(false);
+    range.select();
+  }
+}
