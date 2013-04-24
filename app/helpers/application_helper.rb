ff847f9e4885314d12bee4fb738ce94556ea480e
Adam McCrea <adam@adamlogic.com>
2013-04-23 22:45:03 -0400

    Disable invalid pagination links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,16 @@ module ApplicationHelper
     {class: css_class} unless css_class.blank?
   end
 
+  def link_to_review(link_text, opts = {})
+    position  = opts.delete(:position)
+    css_class = opts.delete(:class)
+
+    if (1..version.reviews.count).include?(position)
+      link_to link_text, doc_path(doc, position: position), class: css_class
+    else
+      css_class << ' disabled'
+      content_tag :span, link_text, class: css_class
+    end
+  end
+
 end
